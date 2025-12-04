64d283ac94138c122d25f7a9e370d40f2a68c260
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-04 11:40:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ForLoopAdding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int j, sum = 0;
+
+            for (j = 1; j <= 10 ; j++)
+            {
+                sum = sum + j;
+                Console.WriteLine($"Summa on: " + sum);
+            }
+        }
+    }
+}
